46b574e556d14309abfe439116a91f5096701ce4
fogcell <z.fogmain@gmail.com>
2016-08-13 09:05:50 +0800

    Add files via upload

---
+++ quicksort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int n;
+int a[6] ={14, 18, 16, 13, 15, 17};
+
+void quicksort(int left,int right)
+{
+    int i,j,t,temp;
+    if(left>right)
+        return;
+    temp=a[left];
+    i=left;
+    j=right;
+    while(i!=j)
+    {
+        while(a[j]>=temp && i<j)
+            j--;
+        while(a[i]<=temp && i<j)
+            i++;
+        if(i<j)//严格检查是否相遇
+        {
+            t = a[i];
+            a[i]=a[j];
+            a[j] = t;
+        }
+    }
+        a[left]=a[i];
+        a[i]=temp;
+        quicksort(left,i-1);
+        quicksort(i+1,right);
+
+}
+void main(void)
+{
+
+   int i;
+   quicksort(0,5);
+   for (i=0;i<6;i++)
+   {
+       printf("%d ",a[i]);
+   }
+
+
+
+}
