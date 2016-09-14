908cb592ca0fac58744aa2dec95f3b8b76473bf5
fogcell <z.fogmain@gmail.com>
2016-09-14 08:55:09 +0800

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -5,7 +5,7 @@ int a[6] ={14, 18, 16, 13, 15, 17};
 void quicksort(int left,int right)
 {
     int i,j,t,temp;
-    if(left>right)
+    if(left>right)//递归结束条件
         return;
     temp=a[left];
     i=left;
@@ -14,7 +14,7 @@ void quicksort(int left,int right)
     {
         while(a[j]>=temp && i<j)
             j--;
-        while(a[i]<=temp && i<j)
+        while(a[i]<temp && i<j)
             i++;
         if(i<j)//严格检查是否相遇
         {
